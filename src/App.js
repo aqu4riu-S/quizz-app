190a582000b36436e6083b6b6cc268721475de0c
Bruno Campos <bmvc98@gmail.com>
2024-03-23 16:30:14 +0000

    feat - dynamic number of rounds and players

--- src/App.js
+++ src/App.js
@@ -3,26 +3,23 @@ import { useState } from "react";
 import PersonsList from "./PersonsList.js";
 import Button from "./Button.js";
 import quiz from "./quiz.js";
+import Form from "./Form.js";
 
 function App() {
-  const [playersPoints, setPlayersPoints] = useState([
-    { name: "Victória", points: 0, id: 0 },
-    { name: "Diogo", points: 0, id: 1 },
-    { name: "Gabriel", points: 0, id: 2 },
-    { name: "João", points: 0, id: 3 },
-  ]);
+  const [playersPoints, setPlayersPoints] = useState([]);
+  const [noRounds, setNoRounds] = useState(4);
 
   const [questionsAnswered, setQuestionsAnswered] = useState(0);
   const [round, setRound] = useState(1);
   const [playerIndex, setPlayerIndex] = useState(0);
   const [activeAnswer, setActiveAnswer] = useState(null);
 
-  const [isGameOn, setIsGameOn] = useState(true);
+  const [isGameOn, setIsGameOn] = useState(false);
   const noQuestions = quiz.questions.length;
 
   const [winner, setWinner] = useState("");
 
-  const [isSetup, setIsSetup] = useState(false);
+  const [isSetup, setIsSetup] = useState(true);
   const [noPlayers, setNoPlayers] = useState(2);
 
   function findWinner(updatedPoints) {
@@ -82,101 +79,24 @@ function App() {
     !isToAdd && noPlayers > 2 && setNoPlayers(noPlayers - 1);
   }
 
+  function handleStartGame(noRounds, playersList) {
+    // { name: "Bruno", points: 0, id: 0}
+    let playersArr = [];
+    Object.keys(playersList).forEach((key, idx) =>
+      playersArr.push({ name: playersList[key], points: 0, id: idx })
+    );
+
+    setNoRounds(noRounds);
+    setPlayersPoints(playersArr);
+    setIsSetup(false);
+  }
+
   return (
-    <div className="App bg-blue-500 text-white px-16 h-screen flex items-center gap-16">
-      {isSetup && (
-        <div className="text-black w-2/4">
-          <form action="">
-            <div className="text-black mb-8">
-              <label for="noRounds" className="mr-4 text-2xl">
-                Número de rondas:
-              </label>
-              <select id="noRounds" name="noRounds">
-                {Array.from({ length: 7 }, (_, index) => index + 4).map(
-                  (idx) => (
-                    <option key={idx} value={idx}>
-                      {idx}
-                    </option>
-                  )
-                )}
-              </select>
-            </div>
-            <div>
-              <div className="flex justify-between items-center mb-6">
-                <button
-                  onClick={(e) => handleAddPlayer(e, false)}
-                  className="btn-sm"
-                >
-                  -
-                </button>
-                <button
-                  onClick={(e) => handleAddPlayer(e, true)}
-                  className="btn-sm"
-                >
-                  +
-                </button>
-              </div>
-              {Array.from({ length: noPlayers }, (_, index) => index + 1).map(
-                (idx) => (
-                  <input
-                    key={idx}
-                    type="text"
-                    placeholder={`Jogador ${idx}`}
-                    className="w-full mb-6 p-4 rounded-lg"
-                  />
-                )
-              )}
-            </div>
-            <div className="flex justify-center">
-              <button className="btn-secondary">Começar o jogo</button>
-            </div>
-          </form>
-        </div>
-      )}
+    <div className="App">
+      {isSetup && <Form onHandleStartGame={handleStartGame} />}
 
-      {isGameOn && (
-        <>
-          <div className="main w-2/3 p-8 bg-white text-black rounded-xl">
-            <div className="header flex justify-between items-center text-lg">
-              <p className="text-blue-600 font-bold text-xl">
-                {playersPoints.at(playerIndex).name}
-              </p>
-              <p>Perguntas respondidas: {questionsAnswered}</p>
-              <p>
-                Ronda{" "}
-                <span className="text-xl text-blue-600 font-bold pr-1">
-                  {round}/
-                </span>
-                <span className="text-sm text-gray-400">4</span>
-              </p>
-            </div>
-            <div className="question my-6 text-3xl">
-              <p>{quiz.questions[questionsAnswered].question}</p>
-            </div>
-            <AnswersList
-              questionObj={quiz.questions[questionsAnswered]}
-              onHandleClickAnswer={handleClickAnswer}
-              activeAnswer={activeAnswer}
-            />
-            <Button
-              onHandleSubmitAnswer={handleSubmitAnswer}
-              activeAnswer={activeAnswer}
-            />
-          </div>
-          <div className="aside bg-white text-black w-1/3 p-8 rounded-lg">
-            <PersonsList personsLst={playersPoints} />
-          </div>
-        </>
-      )}
-
-      {!isGameOn && !isSetup && (
-        <div className="aside bg-white text-black grow p-4 rounded-lg">
-          <PersonsList personsLst={playersPoints} />
-          <h1 className="text-3xl text-center text-blue-800 font-bold">
-            🏆 {winner.join(", ")} 🥇
-          </h1>
-        </div>
-      )}
+      {!isSetup && isGameOn}
+      {!isSetup && !isGameOn}
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+export default function Form({ onHandleStartGame }) {
+  const [noRounds, setNoRounds] = useState(4);
+  const [playersList, setPLayersList] = useState(
+    Object.fromEntries(
+      Array.from({ length: 2 }, (_, i) => [`Player ${i + 1}`, ""])
+    )
+  );
+
+  const [noPlayers, setNoPlayers] = useState(2);
+
+  function handleAddPlayer(e, isToAdd) {
+    e.preventDefault();
+
+    if (isToAdd && noPlayers < 6) {
+      setNoPlayers(noPlayers + 1);
+      setPLayersList({ ...playersList, [`Player ${noPlayers + 1}`]: "" });
+    }
+    if (!isToAdd && noPlayers > 2) {
+      setNoPlayers(noPlayers - 1);
+      const updatedObj = () => {
+        const newObj = { ...playersList };
+        delete newObj[`Player ${noPlayers}`];
+        return newObj;
+      };
+      setPLayersList(updatedObj);
+    }
+  }
+
+  function handlePlayerNameChange(e) {
+    const { name, value } = e.target;
+    setPLayersList({ ...playersList, [name]: value });
+  }
+
+  return (
+    <div className="bg-blue-500 text-white px-16 py-8 h-screen flex justify-center">
+      <div className="text-black w-2/4 h-full bg-gray-200 p-8 rounded-xl">
+        <form action="" className="h-full">
+          <div className="text-black mb-8">
+            <label className="mr-4 text-2xl">Número de rondas:</label>
+            <select
+              id="noRounds"
+              name="noRounds"
+              value={noRounds}
+              onChange={(e) => setNoRounds(e.target.value)}
+            >
+              {Array.from({ length: 7 }, (_, index) => index + 4).map((idx) => (
+                <option key={idx} value={idx}>
+                  {idx}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <div className="flex justify-between items-center mb-6">
+              <button
+                onClick={(e) => handleAddPlayer(e, false)}
+                className="btn-sm"
+              >
+                -
+              </button>
+              <button
+                type="button"
+                onClick={() => onHandleStartGame(noRounds, playersList)}
+                className="btn-secondary"
+              >
+                Começar o jogo
+              </button>
+              <button
+                onClick={(e) => handleAddPlayer(e, true)}
+                className="btn-sm"
+              >
+                +
+              </button>
+            </div>
+            <div className="flex-row">
+              {Array.from({ length: noPlayers }, (_, index) => index + 1).map(
+                (idx) => (
+                  <input
+                    key={idx}
+                    type="text"
+                    placeholder={`Jogador ${idx}`}
+                    className="w-full mb-6 p-4 rounded-lg"
+                    name={`Player ${idx}`}
+                    value={playersList[`Player ${idx}`]}
+                    onChange={handlePlayerNameChange}
+                  />
+                )
+              )}
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
